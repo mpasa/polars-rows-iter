9262af7b4046c70b8567b451165986ccb0310472
Alex <73347343+letkemann@users.noreply.github.com>
2024-11-22 20:55:03 +0100

    examples and docs updated (#11)

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 # polars-rows-iter
 
-This is a **early state** library for simple and convenient row iteration of polars dataframes.<br>
-Currently are only primitive and string types supported.
-
-Todos:
-
-- Unit-Tests
-- Documentation
-- DType: Date support
-- DType: Binary Support
+Library for simple and convenient row iteration of polars dataframes
 
 ### Example:
 
@@ -16,33 +8,48 @@ Todos:
 use polars::prelude::*;
 use polars_rows_iter::*;
 
-#[derive(Debug, FromDataFrameRow)]
-#[derive(PartialEq)] // for assert_eq
-struct MyRow<'a>
-{
-    #[column("col_a")]
-    a: i32,
-    // the column name defaults to the field name if no explicit name given
-    col_b: &'a str
+fn main() {
+    #[derive(Debug, FromDataFrameRow)]
+    #[derive(PartialEq)] // for assert_eq
+    struct MyRow<'a>
+    {
+        #[column("col_a")]
+        a: i32,
+        // the column name defaults to the field name if no explicit name given
+        col_b: &'a str,
+        col_c: String,
+        #[column("col_d")]
+        optional: Option<f64>
+    }
+   
+    let df = df!(
+            "col_a" => [1i32, 2, 3, 4, 5],
+            "col_b" => ["a", "b", "c", "d", "e"],
+            "col_c" => ["A", "B", "C", "D", "E"],
+            "col_d" => [Some(1.0f64), None, None, Some(2.0), Some(3.0)]
+        ).unwrap();
+   
+    let rows_iter = df.rows_iter::<MyRow>().unwrap(); // ready to use row iterator
+    // collect to vector for assert_eq
+    let rows_vec = rows_iter.collect::<PolarsResult<Vec<MyRow>>>().unwrap();
+   
+    assert_eq!(
+        rows_vec,
+        [
+            MyRow { a: 1, col_b: "a", col_c: "A".to_string(), optional: Some(1.0) },
+            MyRow { a: 2, col_b: "b", col_c: "B".to_string(), optional: None },
+            MyRow { a: 3, col_b: "c", col_c: "C".to_string(), optional: None },
+            MyRow { a: 4, col_b: "d", col_c: "D".to_string(), optional: Some(2.0) },
+            MyRow { a: 5, col_b: "e", col_c: "E".to_string(), optional: Some(3.0) },
+        ]
+    );
 }
 
-let df = df!(
-        "col_a" => [1i32, 2, 3, 4, 5],
-        "col_b" => ["a", "b", "c", "d", "e"]
-    ).unwrap();
-
-let rows_iter = df.rows_iter::<MyRow>().unwrap();
-// unwrap rows and collect to vector for assert_eq
-let rows_vec = rows_iter.map(|row|row.unwrap()).collect::<Vec<MyRow>>();
-
-assert_eq!(
-    rows_vec,
-    [
-        MyRow { a: 1, col_b: "a" },
-        MyRow { a: 2, col_b: "b" },
-        MyRow { a: 3, col_b: "c" },
-        MyRow { a: 4, col_b: "d" },
-        MyRow { a: 5, col_b: "e" },
-    ]
-);
 ```
+
+### Performance
+
+
+
+### Todos
+- Document how to extend for custom types
--- polars-rows-iter/src/lib.rs
+++ polars-rows-iter/src/lib.rs
@@ -4,75 +4,48 @@
 //!
 //! ##### Example: Dataframe without None/null values:
 //! ```rust
-//! use polars::prelude::*;
-//! use polars_rows_iter::*;
+//!use polars::prelude::*;
+//!use polars_rows_iter::*;
 //!
-//! #[derive(Debug, FromDataFrameRow)]
-//! #[derive(PartialEq)] // for assert_eq
-//! struct MyRow<'a>
-//! {
-//!     #[column("col_a")]
-//!     a: i32,
-//!     // the column name defaults to the field name if no explicit name given
-//!     col_b: &'a str
-//! }
-//!
-//! let df = df!(
-//!         "col_a" => [1i32, 2, 3, 4, 5],
-//!         "col_b" => ["a", "b", "c", "d", "e"]
-//!     ).unwrap();
-//!
-//! let rows_iter = df.rows_iter::<MyRow>().unwrap(); // ready to use row iterator
-//! // unwrap rows and collect to vector for assert_eq
-//! let rows_vec = rows_iter.map(|row|row.unwrap()).collect::<Vec<MyRow>>();
-//!
-//! assert_eq!(
-//!     rows_vec,
-//!     [
-//!         MyRow { a: 1, col_b: "a" },
-//!         MyRow { a: 2, col_b: "b" },
-//!         MyRow { a: 3, col_b: "c" },
-//!         MyRow { a: 4, col_b: "d" },
-//!         MyRow { a: 5, col_b: "e" },
-//!     ]
-//! );
+//!fn main() {
+//!    #[derive(Debug, FromDataFrameRow)]
+//!    #[derive(PartialEq)] // for assert_eq
+//!    struct MyRow<'a>
+//!    {
+//!        #[column("col_a")]
+//!        a: i32,
+//!        // the column name defaults to the field name if no explicit name given
+//!        col_b: &'a str,
+//!        col_c: String,
+//!        #[column("col_d")]
+//!        optional: Option<f64>
+//!    }
+//!   
+//!    let df = df!(
+//!            "col_a" => [1i32, 2, 3, 4, 5],
+//!            "col_b" => ["a", "b", "c", "d", "e"],
+//!            "col_c" => ["A", "B", "C", "D", "E"],
+//!            "col_d" => [Some(1.0f64), None, None, Some(2.0), Some(3.0)]
+//!        ).unwrap();
+//!   
+//!    let rows_iter = df.rows_iter::<MyRow>().unwrap(); // ready to use row iterator
+//!    // collect to vector for assert_eq
+//!    let rows_vec = rows_iter.collect::<PolarsResult<Vec<MyRow>>>().unwrap();
+//!   
+//!    assert_eq!(
+//!        rows_vec,
+//!        [
+//!            MyRow { a: 1, col_b: "a", col_c: "A".to_string(), optional: Some(1.0) },
+//!            MyRow { a: 2, col_b: "b", col_c: "B".to_string(), optional: None },
+//!            MyRow { a: 3, col_b: "c", col_c: "C".to_string(), optional: None },
+//!            MyRow { a: 4, col_b: "d", col_c: "D".to_string(), optional: Some(2.0) },
+//!            MyRow { a: 5, col_b: "e", col_c: "E".to_string(), optional: Some(3.0) },
+//!        ]
+//!    );
+//!}
 //! ```
 //! Every row is wrapped with a PolarsError, in case of an unexpected null value the row creation fails and the iterator
 //! returns an Err(...) for the row. One can decide to cancel the iteration or to skip the affected row.
-//!
-//! ##### Example: Dataframe with valid None/null values:
-//! ```rust
-//! use polars::prelude::*;
-//! use polars_rows_iter::*;
-//!
-//! #[derive(Debug, FromDataFrameRow)]
-//! #[derive(PartialEq)] // for assert_eq
-//! struct MyRow<'a>
-//! {
-//!     col_a: i32,
-//!     col_b: Option<&'a str>
-//! }
-//!
-//! let df = df!(
-//!         "col_a" => [1i32, 2, 3, 4, 5],
-//!         "col_b" => [Some("a"), None, Some("c"), None, Some("e")]
-//!     ).unwrap();
-//!
-//! let rows_iter = df.rows_iter::<MyRow>().unwrap(); // ready to use row iterator
-//! // unwrap rows and collect to vector for assert_eq
-//! let rows_vec = rows_iter.map(|row|row.unwrap()).collect::<Vec<MyRow>>();
-//!
-//! assert_eq!(
-//!     rows_vec,
-//!     [
-//!         MyRow { col_a: 1, col_b: Some("a") },
-//!         MyRow { col_a: 2, col_b: None },
-//!         MyRow { col_a: 3, col_b: Some("c") },
-//!         MyRow { col_a: 4, col_b: None },
-//!         MyRow { col_a: 5, col_b: Some("e") },
-//!     ]
-//! );
-//! ```
 //!
 //! ## Supported types
 //!
